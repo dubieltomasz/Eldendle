4b6abf76915912899fb7b2bc027e33277a025013
Tomasz Dubiel <dubieltomasz420@gmail.com>
2025-12-06 18:44:52 +0100

    Basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Input from './input'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Input/>
+      <table>
+        <tr>
+          <th colSpan={2}>Weapon</th>
+          <th>Type</th>
+          <th>Damage</th>
+          <th>Critical Boost</th>
+          <th>Scaling</th>
+          <th>Weight</th>
+          <th>Upgrade Material</th>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Alfreds Futterkiste</td>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Alfreds Futterkiste</td>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+          <td>Germany</td>
+        </tr>
+      </table>
     </>
   )
 }
---
+++ src/input.tsx
@@ -0,0 +1,10 @@
+function input() {
+    return (
+        <form>
+            <input type="text" name="textInput"/>
+            <button type="submit">Submit</button>
+        </form>
+    );  
+};
+
+export default input;
