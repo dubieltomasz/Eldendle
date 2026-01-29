517f1c0ff7d7c37545ce2212472425e76caf077b
Tomasz Dubiel <dubieltomasz420@gmail.com>
2026-01-29 23:41:59 +0100

    Changed routing logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import { Outlet } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import './App.css';
+import WeaponGuesser from './routes/WeaponGuesser';
+import CraftingGuesser from './routes/CraftingGuesser';
+import BossGuesser from './routes/BossGuesser';
 
 function App() {
   return (
     <>
       <Header />
-      <Nav />
-      <Outlet />
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path='/eldendle/' element={<WeaponGuesser />} />
+          <Route path='/eldendle/bossguesser' element={<BossGuesser />} />
+          <Route path='/eldendle/craftingguesser' element={<CraftingGuesser />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
-import WeaponGuesser from './routes/WeaponGuesser';
-import BossGuesser from './routes/BossGuesser.tsx';
-import CraftingGuesser from './routes/CraftingGuesser.tsx';
 import './index.css'
-
-const router = createBrowserRouter(
-  [{
-    path: '/',
-    element: <App />,
-    children: [
-      { index: true, element: <WeaponGuesser /> }
-    ],
-  }, {
-    path: '/bossguesser/',
-    element: <App />,
-    children: [
-      { index: true, element: <BossGuesser /> }
-    ],
-  }, {
-    path: '/craftingguesser/',
-    element: <App />,
-    children: [
-      { index: true, element: <CraftingGuesser /> }
-    ],
-  }],
-  { basename: '/eldendle/', }
-);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
